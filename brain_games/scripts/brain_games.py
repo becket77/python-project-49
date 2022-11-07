56ddbf80f57a868dc78e086fe76d342e8102cd99
Boris Kochergin <boris.kochergin@gmail.com>
2022-11-07 03:56:20 +0300

    small correction

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+import brain_games.cli
+from brain_games.cli import welcome_user
 
 def main():
-    print("Welcome to the Brain Games!")
+    return "Welcome to the Brain Games!"
 
 
 if __name__ == '__main()__':
     main()
-
+    welcome_user()
