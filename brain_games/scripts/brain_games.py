866b16ea10ee3d2cc5f9bdb39f7080398bb67668
Boris Kochergin <boris.kochergin@gmail.com>
2022-11-07 04:02:24 +0300

    correction in script

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,10 +2,11 @@
 import brain_games.cli
 from brain_games.cli import welcome_user
 
+
 def main():
-    return "Welcome to the Brain Games!"
+    print("Welcome to the Brain Games!")
+    welcome_user()
 
 
 if __name__ == '__main()__':
     main()
-    welcome_user()
